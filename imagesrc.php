166998bba5582e68d5a0c395bdeb375770c4d9f7
Kevin LEIBA <kleiba@e2r13p3.42.fr>
2016-07-17 19:17:36 +0200

    ajax request seems good. working on php algorithm to merge images with frames

--- imagesrc.php
+++ imagesrc.php
@@ -5,4 +5,24 @@
   $frame = $_POST['frame'];
 
   print_r($frame);
+
+
+  // $stamp = imagecreatefrompng($imgurl);
+  // $im = imagecreatefrompng('frames/'.$frame.'.png');
+
+  // // Définit les marges pour le cachet et récupère la hauteur et la largeur de celui-ci
+  // $marge_right = 10;
+  // $marge_bottom = 10;
+  // $sx = imagesx($stamp);
+  // $sy = imagesy($stamp);
+
+  // // Copie le cachet sur la photo en utilisant les marges et la largeur de la
+  // // photo originale  afin de calculer la position du cachet 
+  // imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+
+  // // Affichage et libération de la mémoire
+  // header('Content-type: image/png');
+  // imagepng($im);
+  // imagedestroy($im);
+
 ?>
