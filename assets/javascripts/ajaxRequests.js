921a7bcb1a149eea1c9a2b16482a243e69cdd9a9
Kevin LEIBA <kleiba@e1r10p1.42.fr>
2016-07-19 14:48:22 +0200

    code allows to save image by clicking on a frame

--- assets/javascripts/ajaxRequests.js
+++ assets/javascripts/ajaxRequests.js
@@ -1,25 +1,44 @@
-function getImageUrl(frame){
-  // Create our XMLHttpRequest object
-  var hr = new XMLHttpRequest();
-  // Create some variables we need to send to our PHP file
-  var url = "imagesrc.php";
-  // var imgurl = document.getElementById("photo").src;
-  var canvasFinal = document.getElementById("canvas");
-  var imgurl = canvasFinal.toDataURL('image/png');
-  var vars = "imgurl="+imgurl+'&frame='+frame;
-  hr.open("POST", url, true);
-  hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  // Access the onreadystatechange event for the XMLHttpRequest object
-  hr.onreadystatechange = function() {
-      console.log(hr);
+// function getImageUrl(frame){
+//   // Create our XMLHttpRequest object
+//   var hr = new XMLHttpRequest();
+//   // Create some variables we need to send to our PHP file
+//   var url = "imagesrc.php";
+//   // var imgurl = document.getElementById("photo").src;
+//   var canvasFinal = document.getElementById("canvas");
+//   // var imgurl = canvasFinal.toDataURL('image/png');
+//   var imgurl = canvasFinal.toDataURL();
+//   var vars = "imgurl="+imgurl+'&frame='+frame;
+//   hr.open("POST", url, true);
+//   // hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//   hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//   // Access the onreadystatechange event for the XMLHttpRequest object
+//   hr.onreadystatechange = function() {
+//       console.log(hr);
 
-      if(hr.readyState == 4 && hr.status == 200) {
-          var return_data = hr.responseText;
-          document.getElementById("imgsrc").innerHTML = return_data;
-      }
-  }
-  // Send the data to PHP now... and wait for response to update the status div
-  hr.send(vars); // Actually execute the request
-  document.getElementById("imgsrc").innerHTML = "processing...";
-  window.scrollTo(0, 0);
+//       if(hr.readyState == 4 && hr.status == 200) {
+//           var return_data = hr.responseText;
+//           document.getElementById("imgsrc").innerHTML = return_data;
+//       }
+//   }
+//   // Send the data to PHP now... and wait for response to update the status div
+//   hr.send(vars); // Actually execute the request
+//   document.getElementById("imgsrc").innerHTML = "processing...";
+//   window.scrollTo(0, 0);
+// }
+
+function getImageUrl(frame){
+    var base64 = document.getElementById("photo").src;
+    var vars = 'frame='+frame+"&imgurl="+base64;
+    ajax = new XMLHttpRequest();
+    ajax.open( 'POST', 'imagesrc.php', true );
+    ajax.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+    ajax.send( vars );
+    window.scrollTo(0, 0);
+    ajax.onreadystatechange = function() {
+    if (ajax.readyState == 4 && ajax.status == 200) {
+      document.getElementById("imgsrc").innerHTML = ajax.responseText;
+    }}
+
 }
+
+var canvasFinal = document.getElementById('canvas');
--- imagesrc.php
+++ imagesrc.php
@@ -1,52 +1,17 @@
 <?php
-  // header("Content-Type: image/png");
-  include_once 'functions.php';
 
-  $imgurl = $_POST['imgurl'];
-  $frame = $_POST['frame'];
+    $imgurl = $_POST['imgurl'];
+    $frame = $_POST['frame'];
 
-  // $im = imagecreatefrompng($imgurl);
-  // $stamp = imageCreateFromPng('frames/'.$frame.'.png');
-  $filteredData=substr($imgurl, strpos($imgurl, ",")+1);
-  $unencodedData=base64_decode($filteredData);
-  // file_put_contents("tmp/tobocheat.png", $unencodedData);
-  $fp = fopen( "tmp/tibochite.png", 'wb' );
-  fwrite( $fp, $unencodedData);
-  fclose( $fp );
-  
-  //code works
-  // $stamp = imageCreateFromPng('frames/'."violet".'.png');
-  // imagepng($stamp, 'tmp/imagetest.png');
-  //
+    $imgurl = str_replace(" ", '+', $imgurl);
+    $filteredData=substr($imgurl, strpos($imgurl, ",")+1);
 
+    $unencodedData= base64_decode($filteredData);
 
-  die;
-// $encodedString = str_replace(' ','+', $imgurl);
-// $decodedString = base64_decode($encodedString);
-// file_put_contents('tmp/MyFile.png', $decodedString);
+    //echo "unencodedData".$unencodedData;
 
-// $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imgurl));
-// file_put_contents('tmp/image.png', $data);
-//   $encodedData = str_replace(' ','+',$imgurl);
-// $decocedData = base64_decode($encodedData);
-// file_put_contents('tmp/image.png', $decocedData);
-
-
-
-  // die;
-  // Définit les marges pour le cachet et récupère la hauteur et la largeur de celui-ci
-  $marge_right = 10;
-  $marge_bottom = 10;
-  $sx = imagesx($stamp);
-  $sy = imagesy($stamp);
-
-  // Copie le cachet sur la photo en utilisant les marges et la largeur de la
-  // photo originale  afin de calculer la position du cachet 
-  imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
-
-  // Affichage et libération de la mémoire
-  header('Content-type: image/png');
-  imagepng($im);
-  imagedestroy($im);
+    $fp = fopen( 'tmp/last.png', 'wb' );
+    fwrite( $fp, $unencodedData);
+    fclose( $fp );
 
 ?>
