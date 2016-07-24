cf31fcd0fd7b4c0eafe6a6e6923c41071cfbed67
Kevin LEIBA <kleiba@e2r8p1.42.fr>
2016-07-24 15:16:32 +0200

    added emoji on db

--- config/setup.php
+++ config/setup.php
@@ -3,14 +3,14 @@
   session_start();
 
   $dsn = explode(';', $DB_DSN);
-  $db = "camagruuu";
+  $db = "camagru";
 
   $create = "CREATE DATABASE `$db`;";
   $user = "CREATE TABLE `user`(
   `id` int(10) NOT NULL,
   `email` tinytext NOT NULL,
-  `password` tinytext NOT NULL,
-  `username` tinytext NOT NULL,
+  `password` tinytext CHARACTER SET utf8mb4 NOT NULL,
+  `username` tinytext CHARACTER SET utf8mb4 NOT NULL,
   `token` varchar(32) NOT NULL,
   `valid` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (id));
@@ -21,13 +21,13 @@
   `uid` tinytext NOT NULL,
   `user_id` int(10) NOT NULL,
   `likes` text NOT NULL,
-  `comments` varchar(32) NOT NULL,
+  `comments` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (id));
   ALTER TABLE `picture` MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;";
 
   $comment = "CREATE TABLE `comment`(
   `id` int(10) NOT NULL,
-  `comment` tinytext NOT NULL,
+  `comment` tinytext CHARACTER SET utf8mb4 NOT NULL,
   `user_id` int(10) NOT NULL,
   `picture_uid` text NOT NULL,
   PRIMARY KEY (id));
@@ -45,13 +45,7 @@
 
     $bdd->exec($user.$picture.$comment) 
     or die(print_r($dbh->errorInfo(), true));
-
-    // $bdd->exec($picture) 
-    // or die(print_r($dbh->errorInfo(), true));
-
-    // $bdd->exec($comment) 
-    // or die(print_r($dbh->errorInfo(), true));        
-
+      
     header("location: ../index.php");
   }
   catch (PDOException $e)
